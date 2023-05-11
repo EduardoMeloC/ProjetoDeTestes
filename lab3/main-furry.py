573cdfaed5419321da120dc90f6fc2ffd3409858
Rafael de Aguiar <kuati247@dcc.ufrj.br>
2023-05-11 00:27:50 -0300

    Lab3 - Update banco.py

--- lab3/main-furry.py
+++ lab3/main-furry.py
@@ -13,29 +13,27 @@ def argmin(a):
     return min(range(len(a)), key=lambda x : a[x])
 
 def banco(C, arrival, service):
-    cashier = []
-    count = 0
-
     #perhaps sort arrival and service by arrival
     last_arrival = 0
     cashier = [0]*C
     waited = 0
     argwaited = 0
+    count = 0
 
     for t in range(len(arrival)):
         diff = arrival[t]-last_arrival
-        print(f"{t} arrived {diff}s after {t-1}")
+        #print(f"{t} arrived {diff}s after {t-1}")
         last_arrival = arrival[t]
         if(diff>0):
             cashier = [max(x - diff,0) for x in cashier]
-        print(f"cashiers = {cashier}")
+        #print(f"cashiers = {cashier}")
         argwaited = argmin(cashier)
         waited = cashier[argwaited]
         if(waited<=0):
-            print(f"{t} got served")
+            #print(f"{t} got served")
             cashier[argwaited]=service[t]
         else:
-            print(f"{t} waited {waited} to get served by {argwaited}")
+            #print(f"{t} waited {waited} to get served by {argwaited}")
             if(waited>20):
                 count += 1
             cashier[argwaited]+=(service[t])
