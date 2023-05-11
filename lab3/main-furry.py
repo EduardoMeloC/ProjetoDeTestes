a5c1d7ddce15fd2515a8144711f50f09a8593ae0
Rafael de Aguiar <kuati247@dcc.ufrj.br>
2023-05-10 23:36:11 -0300

    L3 - Corrige formato de entrada

--- lab3/main-furry.py
+++ lab3/main-furry.py
@@ -1,25 +1,20 @@
-def argmin(a):
-    return min(range(len(a)), key=lambda x : a[x])
-
-def banco():
+def main():
     arrival = []
     service = []
-    cashier = []
-    waiting = []
-
-    count = 0
-
     C,N = [int(x) for x in input().split()]
-    
     for i in range(N):
         T,D = [int(x) for x in input().split()]
         arrival.append(T)
         service.append(D)
     
-    print(C)
-    print(N)
-    print(arrival)
-    print(service)
+    print(banco(C, arrival, service))
+
+def argmin(a):
+    return min(range(len(a)), key=lambda x : a[x])
+
+def banco(C, arrival, service):
+    cashier = []
+    count = 0
 
     #perhaps sort arrival and service by arrival
     last_arrival = 0
@@ -27,34 +22,24 @@ def banco():
     waited = 0
     argwaited = 0
 
-    for t in range(N):
-        diff = arrival[t]-last_arrival #-waited
+    for t in range(len(arrival)):
+        diff = arrival[t]-last_arrival
         print(f"{t} arrived {diff}s after {t-1}")
         last_arrival = arrival[t]
         if(diff>0):
             cashier = [max(x - diff,0) for x in cashier]
         print(f"cashiers = {cashier}")
-        waited = min(cashier)
         argwaited = argmin(cashier)
-        for c in range(C):
-            succ = False
-            if(cashier[c]<=0):
-                print(f"{t} got served")
-                cashier[c]=service[t]
-                succ = True
-                break
-        if(not succ):
+        waited = cashier[argwaited]
+        if(waited<=0):
+            print(f"{t} got served")
+            cashier[argwaited]=service[t]
+        else:
             print(f"{t} waited {waited} to get served by {argwaited}")
-            #waiting.append([t,0])
             if(waited>20):
                 count += 1
             cashier[argwaited]+=(service[t])
-    print(count)
 
+    return count
 
-            
-
-
-        
-
-banco()
+main()
